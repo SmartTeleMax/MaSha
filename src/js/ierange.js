2917ff0ba71f5c2c804aef630e2e20782fba1c06
Anton Byrna <it.release@gmail.com>
2011-08-03 18:18:02 +0400

    update getTextNodes

--- src/js/ierange.js
+++ src/js/ierange.js
@@ -248,8 +248,7 @@ DOMRange.prototype = {
 	getTextNodes_: function(node){
     for(var i in node.childNodes){
       if(node.childNodes[i].nodeType == 3){
-        this._textNodes[c] = node.childNodes[i];
-        c++;
+        this._textNodes.push(node.childNodes[i]);
       }else this.getTextNodes(node.childNodes[i]);
     }
   },
