2365533708ee01d1c3987733cd30b19a9ba800e8
Harut <Harut>
2012-02-15 15:09:56 +0400

    updated masha, added packer scripts

--- js/common.js
+++ js/common.js
@@ -1,44 +1,18 @@
-var options = {
-
-}
-if (window.addEventListener){
-    window.addEventListener('load', function(){
-        // can be called by domready
-        MaSha.instance = new MaSha({
-            ignored: function(arg){
-                if ($(arg).attr('id') == 'projects' || $(arg).parents('#projects').length ||
-                    $(arg).attr('id') == 'plugins' || $(arg).parents('#plugins').length ||
-                    $(arg).attr('id') == 'download' || $(arg).parents('#download').length ||
-                    $(arg).hasClass('code') || $(arg).parents('.code:first').length) {
-                    console.log(arg);
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-    }, false);
-} else {
-    window.attachEvent('onload', function(){
-        // can be called by domready
-        MaSha.instance = new MaSha({
-            ignored: function(arg){
-                
-                if ($(arg).attr('id') == 'projects' || $(arg).parents('#projects').length ||
-                    $(arg).attr('id') == 'plugins' || $(arg).parents('#plugins').length ||
-                    $(arg).attr('id') == 'download' || $(arg).parents('#download').length ||
-                    $(arg).hasClass('code') || $(arg).parents('.code:first').length) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-    });
-}
-
-
 $(document).ready(function(){
+    MaSha.instance = new MaSha({
+        'select_message': 'upmsg-selectable',
+        ignored: function(arg){
+            if ($(arg).attr('id') == 'projects' || $(arg).parents('#projects').length ||
+                $(arg).attr('id') == 'plugins' || $(arg).parents('#plugins').length ||
+                $(arg).attr('id') == 'download' || $(arg).parents('#download').length ||
+                $(arg).hasClass('code') || $(arg).parents('.code:first').length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+
     var check = $(window).height() - 660;
     var stoped = false;
     $('#tweets').jTweetsAnywhere({
@@ -55,12 +29,9 @@ $(document).ready(function(){
            	showTimestamp: false,
         },
         tweetFilter: function(bb){
-            //console.log(bb);
             var ttweet = $('<li class="jta-tweet-list-item forremove"><div class="jta-tweet-body "><span class="jta-tweet-text">'+bb.text+'</span><span class="jta-tweet-attributes"></span></div><div class="jta-clear">&nbsp;</div></li>');
             $('.jta-tweet-list').append($(ttweet).css('visibility', 'hidden'));
             
-            //console.log('height', $('.jta-tweet-list').height());
-            //console.log(check - $('.jta-tweet-list').height());
             if (check - $('.jta-tweet-list').height() > 0 && !stoped) {
                 return true;
             } else {
@@ -91,9 +62,8 @@ $(document).ready(function(){
     
     $('.menu a').bind('click', function(){
         var _id = $(this).attr('href').split('#')[1];
-        console.log(_id);
         $(window).scrollTo( $('#'+_id), 800, {offset:-70} );
         return false;
     });
     
-})
+});
